a7eae521bd1b8e5641138914288b58b7d7004e6d
Kooostenka <kostyakw@mail.ru>
2022-03-25 08:19:03 +0500

    release v1

---
+++ resources/views/home.blade.php
@@ -0,0 +1,19 @@
+@extends('layouts.app')
+
+@section('title-block')
+    Главная страница
+@endsection
+
+
+
+@section('content')
+    <h1>Главная страница</h1>
+    <p>Lorem ipsum dolor sit amet, consectetur adipisicing elit. Architecto eum ipsa ipsum magnam nisi omnis quasi quisquam sunt, totam ut. Beatae consectetur culpa fuga obcaecati officiis sit, tempore veniam. Doloribus mollitia nostrum, odit pariatur possimus quaerat! Animi architecto aspernatur autem blanditiis consequatur cum deleniti doloremque doloribus ea esse excepturi explicabo facere fuga harum illo inventore iusto libero magnam magni molestiae necessitatibus neque odit pariatur perspiciatis quas, quia quibusdam repellendus sed ut voluptatibus! A dicta doloremque dolorum eius numquam quasi ullam. Autem cum eligendi hic, iure maxime minus neque nihil non odit perferendis provident quas quidem repellat, tempora temporibus tenetur, voluptas.</p>
+
+@endsection
+
+
+@section('aside')
+    @parent
+    <p>Дополнительный текст</p>
+@endsection
---
+++ resources/views/inc/hero.blade.php
@@ -0,0 +1,6 @@
+<div class="jumbotron">
+    <div class="container">
+        <h1>Привет всем на нашем сайте!</h1>
+        <p>Lorem ipsum dolor sit amet, consectetur adipisicing elit. Ad deleniti ea ipsa nostrum quia quod repellat. Aliquid atque aut beatae corporis cumque dolores ducimus ex excepturi id iusto laboriosam modi nihil nulla quaerat, quisquam reiciendis, veritatis? Accusantium ad corporis, cupiditate et fugiat illum labore magnam, nobis possimus quis repudiandae vitae!</p>
+    </div>
+</div>
